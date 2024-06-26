c8bb3239c4cc4207c8e820649e5b8cb73f0efa72
ricardonicolasv <102821061+ricardonicolasv@users.noreply.github.com>
2024-06-26 01:02:25 -0400

    ajustes para crear pedidos

--- jardineria/jardineria_app/urls.py
+++ jardineria/jardineria_app/urls.py
@@ -1,36 +1,32 @@
+#urls.py
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import (
-    admin_page, carro, flores, base, listaprod, maceteros, nuevopd,
-    nuevousuario, pedidosad, pedidoscli, recuperar, recuperardos,
-    suculentas, sustratos, tierra, usuarios, home, crearcuenta, salir, producto,crearproducto,detalles_producto,modificarproducto
+     carro, eliminar_del_carro, procesar_pago, vaciar_carro,agregar_al_carro, flores, base, maceteros, pedido, 
+    suculentas, sustratos, tierra,  home, crearcuenta, salir, producto,crearproducto,detalles_producto,modificarproducto
 )
 
-urlpatterns = [
-    path('administrador/', admin_page, name='admin_page'), 
+urlpatterns = [ 
     path('carro/', carro, name='carro'),
+    path('carro/agregar/<int:codigo_producto>/', agregar_al_carro, name='agregar_al_carro'),
+    path('carro/eliminar/<int:id>/', eliminar_del_carro, name='eliminar_del_carro'),
+    path('carro/procesar_pago/', procesar_pago, name='procesar_pago'),
+    path('carro/vaciar/', vaciar_carro, name='vaciar_carro'),
     path('flores/', flores, name='flores'),
     path('base/', base, name='base'),
     path('', home, name='home'),
-    path('listaprod/', listaprod, name='listaprod'),
     path('maceteros/', maceteros, name='maceteros'),
-    path('nuevopd/', nuevopd, name='nuevopd'),
-    path('nuevousuario/', nuevousuario, name='nuevousuario'),
-    path('pedidosad/', pedidosad, name='pedidosad'),
-    path('pedidoscli/', pedidoscli, name='pedidoscli'),
-    path('recuperar/', recuperar, name='recuperar'),
-    path('recuperardos/', recuperardos, name='recuperardos'),
+    path('pedidoscli/', pedido, name='pedidoscli'),
     path('crearcuenta/', crearcuenta, name='crearcuenta'),
     path('suculentas/', suculentas, name='suculentas'),
     path('sustratos/', sustratos, name='sustratos'),
     path('tierra/', tierra, name='tierra'),
-    path('usuarios/', usuarios, name='usuarios'),
     path('salir/',salir,name='salir'),
     path('productos/',producto, name='productos'),
     path('detalles_producto/<id>',detalles_producto, name='detalles_producto'),
     path('modificarproducto/<id>',modificarproducto, name='modificarproducto'),
-    path('crearproducto/',crearproducto, name='crearproducto')
+    path('crearproducto/',crearproducto, name='crearproducto'),
 ]
 
 if settings.DEBUG:
--- jardineria/jardineria_app/views.py
+++ jardineria/jardineria_app/views.py
@@ -1,63 +1,140 @@
+#views.py
 from django.shortcuts import render
-from datetime import date, datetime
 from django.shortcuts import get_object_or_404, redirect
 from .forms import  UserForm,ProductoForm,UpdProductoForm
 from django.contrib import messages
-from os import remove, path
-from django.conf import settings
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from .models import Producto
+from .models import Producto,Pedido
+from .tipos import TIPO_PRODUCTO
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
-def admin_page(request):
-    return render(request, 'crud/administrador.html')
 
-def carro(request):
-    return render(request, 'crud/carro.html')
-
+@login_required
 def flores(request):
-    return render(request, 'crud/flores.html')
-
-def base(request):
-    return render(request, 'crud/base.html')
-
-def listaprod(request):
-    return render(request, 'crud/listaprod.html')
-
+    productos_flores = Producto.objects.filter(tipo='FLORES')
+    datos = {
+        "productos_flores": productos_flores
+    }
+    return render(request, 'crud/flores.html', datos)
+@login_required
 def maceteros(request):
-    return render(request, 'crud/maceteros.html')
-
-def nuevopd(request):
-    return render(request, 'crud/nuevopd.html')
-
-def nuevousuario(request):
-    return render(request, 'crud/nuevousuario.html')
-
-def pedidosad(request):
-    return render(request, 'crud/pedidosad.html')
-
-def pedidoscli(request):
-    return render(request, 'crud/pedidoscli.html')
-
-def recuperar(request):
-    return render(request, 'crud/recuperar.html')
-
-def recuperardos(request):
-    return render(request, 'crud/recuperardos.html')
-
+    productos_maceteros = Producto.objects.filter(tipo='MACETEROS')
+    datos = {
+        "productos_maceteros": productos_maceteros
+    }
+    return render(request, 'crud/maceteros.html', datos)
+@login_required
 def suculentas(request):
-    return render(request, 'crud/suculentas.html')
-
+    productos_suculentas = Producto.objects.filter(tipo='SUCULENTAS')
+    datos = {
+        "productos_suculentas": productos_suculentas
+    }
+    return render(request, 'crud/suculentas.html', datos)
+@login_required
 def sustratos(request):
-    return render(request, 'crud/sustratos.html')
-
+    productos_sustratos = Producto.objects.filter(tipo='SUSTRATOS')
+    datos = {
+        "productos_sustratos": productos_sustratos
+    }
+    return render(request, 'crud/sustratos.html', datos)
+@login_required
 def tierra(request):
-    return render(request, 'crud/tierra.html')
+    productos_tierras = Producto.objects.filter(tipo='TIERRA DE HOJA')
+    datos = {
+        "productos_tierras": productos_tierras
+    }
+    return render(request, 'crud/tierra.html', datos)
+def producto (request):
+    producto=Producto.objects.all()
 
-def usuarios(request):
-    return render(request, 'crud/usuarios.html')
-def home(request):
-    return render(request, 'crud/home.html')
+    datos={
+        "producto":producto
+    }
+    return render(request,'crud/productos.html', datos)
+
+@login_required
+def crearproducto(request):
+    formulario=ProductoForm()
+    if request.method=="POST":
+        formulario=ProductoForm(request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            #from django.contrib import messages
+            messages.set_level(request,messages.SUCCESS)
+            messages.success(request, "Producto creado con exito!!!")
+            return redirect(to="productos")
+        
+    datos={
+        "formulario":formulario
+    }
+    return render(request,'crud/crearproducto.html', datos)
+@login_required
+def detalles_producto(request, id):
+    #persona=Persona.objects.get(rut=id)
+    producto=get_object_or_404(Producto,codigo_producto=id)
+    datos={
+        "producto":producto
+    }
+    return render(request,'crud/detalles_producto.html',datos)
+@login_required
+def modificarproducto(request,id):
+    producto=get_object_or_404(Producto, codigo_producto=id)
+    form=UpdProductoForm(instance=producto)
+    if request.method=="POST":
+         form=UpdProductoForm(request.POST, files=request.FILES, instance=producto)
+         if form.is_valid():
+             form.save()
+             messages.set_level(request,messages.WARNING)
+             messages.warning(request,"Producto modificado")
+             return redirect(to="productos")
+    datos={
+        'producto':producto,
+        'form':form
+    }
+    return render(request,'crud/modificarproducto.html',datos)
+
+@login_required
+def carro(request, id):
+    producto = get_object_or_404(Producto, codigo_producto=id)
+    pedido, creado = Pedido.objects.get_or_create(usuario=request.user, producto=producto)
+    if not creado:
+        pedido.cantidad += 1
+        pedido.save()
+    messages.success(request, f"{producto.nombre_producto} ha sido añadido al carro.")
+    
+    # Redirigir a la página anterior
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+@login_required
+def eliminar_del_carro(request, id):
+    pedido = get_object_or_404(Pedido, id=id, usuario=request.user)
+    pedido.delete()
+    messages.success(request, 'Producto eliminado del carro.')
+    return redirect('carro')
+
+@login_required
+def procesar_pago(request):
+    # Lógica para procesar el pago
+    messages.success(request, 'Pago procesado correctamente.')
+    return redirect('carro')
+
+@login_required
+def vaciar_carro(request):
+    Pedido.objects.filter(usuario=request.user).delete()
+    messages.success(request, 'Carro vaciado correctamente.')
+    return redirect('carro')
+
+@login_required
+def agregar_al_carro(request, codigo_producto):
+    producto = get_object_or_404(Producto, codigo_producto=codigo_producto)
+    pedido, created = Pedido.objects.get_or_create(usuario=request.user, producto=producto)
+    if not created:
+        pedido.cantidad += 1
+        pedido.save()
+    messages.success(request, 'Producto agregado al carro.')
+    return redirect('carro')
 
 def crearcuenta(request):
     form=UserForm()
@@ -75,60 +152,38 @@ def salir(request):
     logout(request)
     return redirect(to='home')
 
-def producto(request):
-    productos = Producto.objects.all()
 
-    # Filtrar productos de flores
-    productos_flores = productos.filter(tipo='flores')
+def admin_page(request):
+    return render(request, 'crud/administrador.html')
 
-    datos = {
-        "productos_generales": productos,
-        "productos_flores": productos_flores
-    }
+def carro(request):
+    return render(request, 'crud/carro.html')
 
-    if request.path == '/productos/':
-        return render(request, 'crud/productos.html', datos)
-    elif request.path == '/flores/':
-        return render(request, 'crud/flores.html', datos)
-    else:
-        return render(request, 'error.html', {'message': 'Página no encontrada'})
+def base(request):
+    return render(request, 'crud/base.html')
 
-def crearproducto(request):
-    formulario=ProductoForm()
-    if request.method=="POST":
-        formulario=ProductoForm(request.POST, files=request.FILES)
-        if formulario.is_valid():
-            formulario.save()
-            #from django.contrib import messages
-            messages.set_level(request,messages.SUCCESS)
-            messages.success(request, "Producto creado con exito!!!")
-            return redirect(to="productos")
-        
-    datos={
-        "formulario":formulario
-    }
-    return render(request,'crud/crearproducto.html', datos)
+def listaprod(request):
+    return render(request, 'crud/listaprod.html')
 
-def detalles_producto(request, id):
-    #persona=Persona.objects.get(rut=id)
-    producto=get_object_or_404(Producto,codigo_producto=id)
-    datos={
-        "producto":producto
-    }
-    return render(request,'crud/detalles_producto.html',datos)
+def nuevopd(request):
+    return render(request, 'crud/nuevopd.html')
 
-def modificarproducto(request,id):
-    producto=get_object_or_404(Producto, codigo_producto=id)
-    form=UpdProductoForm(instance=producto)
-    if request.method=="POST":
-         form=UpdProductoForm(request.POST, files=request.FILES, instance=producto)
-         if form.is_valid():
-             form.save()
-             messages.set_level(request,messages.WARNING)
-             messages.warning(request,"Producto modificado")
-             return redirect(to="productos")
-    datos={
-        'producto':producto,
-        'form':form
-    }
-    return render(request,'crud/modificarproducto.html',datos)
+def nuevousuario(request):
+    return render(request, 'crud/nuevousuario.html')
+
+def pedidosad(request):
+    return render(request, 'crud/pedidosad.html')
+
+def pedido(request):
+    return render(request, 'crud/pedidoscli.html')
+
+def recuperar(request):
+    return render(request, 'crud/recuperar.html')
+
+def recuperardos(request):
+    return render(request, 'crud/recuperardos.html')
+
+def usuarios(request):
+    return render(request, 'crud/usuarios.html')
+def home(request):
+    return render(request, 'crud/home.html')
